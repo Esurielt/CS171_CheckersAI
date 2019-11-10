39215268a4934d6cf0d35c5f16fc7bc56a3bc073
esurielt <esurielt9917@gmail.com>
2019-11-10 02:50:49 -0800

    Test 2
    working a-b pruning.

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -14,10 +14,9 @@ class StudentAI:
         self.p = p
         self.board = Board(col, row, p)
         self.board.initialize_game()
-        # color {black: 1, white: 2}
+        self.colors_dict = {1: 'B', 2: 'W'}
         self.opponent = {1: 2, 2: 1}
         self.color = 2
-        self.true_color = 2
 
     def get_move(self, move):
         """
@@ -29,14 +28,11 @@ class StudentAI:
             self.board.make_move(move, self.opponent[self.color])
         else:
             self.color = 1
-        moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0, len(moves) - 1)
-        inner_index = randint(0, len(moves[index]) - 1)
-        move = moves[index][inner_index]
-        self.board.make_move(move, self.color)
+        move = self.alpha_beta(self.board, 10, (Move([]), -math.inf), (Move([]), math.inf), True)[0]
+        print(move)
         return move
 
-    def eval(self, board: Board):
+    def evaluate(self, board: Board):
         """
         Return the state/heuristic of the current board according to self.color.
         State Params:
@@ -52,9 +48,14 @@ class StudentAI:
         :param board: the Board being evaluate.
         :return: a int representing the heuristic
         """
-        pass
+        coefficient = [1, -1][self.color-1]
+        population = 3*(board.black_count - board.white_count)
+        kingdom = self.kingdom_calc(board)
+        lords = 2*(kingdom[0] - kingdom[1])     # kings worth ~5
+        walls = 3*(kingdom[2] - kingdom[3])     # walls worth 7
+        return coefficient*(population + lords + walls)
 
-    def alpha_beta(self, board: Board, depth: int, alpha: int, beta: int, max_player: bool):
+    def alpha_beta(self, board: Board, depth: int, alpha: (Move,int), beta: (Move,int), max_player: bool):
         """
         Traditional Alpha-Beta pruning.
         :param board: current board
@@ -64,5 +65,45 @@ class StudentAI:
         :param max_player: whether the current color of players is the one being maxed
         :return: best Move.
         """
-        # initial call: alpha_beta(board, 3, -math.inf, math.inf, true)
-        pass
+        # initial call: alpha_beta(board, 3, (Move([]),-math.inf), (Move([]),math.inf), true)
+        if depth == 0 or board.is_win('B') or board.is_win('W'):
+            return Move([]), self.evaluate(board)
+        if max_player:
+            max_h = -math.inf
+            for child in board.get_all_possible_moves(self.color):
+                print(child)
+                board.make_move(child, self.colors_dict[self.color])
+                h = self.alpha_beta(board, depth - 1, alpha, beta, False)
+                max_h = max(max_h[1], h[1])
+                alpha = max(alpha, max_h[1])
+                if beta <= alpha:
+                    break
+            return max_h
+        else:
+            min_h = math.inf
+            for child in board.get_all_possible_moves(self.opponent[self.color]):
+                board.make_move(child, self.colors_dict[self.color])
+                h = self.alpha_beta(board, depth - 1, alpha, beta, True)
+                min_h = min(min_h[1], h[1])
+                beta = min(min_h[1], beta[1])
+                if beta <= alpha:
+                    break
+            return min_h
+
+    def kingdom_calc(self, board):
+        """
+        Count the Kings for the specific color.
+        :param board: current board
+        :return: int tuple of (black_kings, white_kings, black_guards, white_guards)
+        """
+        kingdoms = [0, 0, 0, 0]
+        colors_dict = {'B': 0, 'W': 1}
+        for r in range(board.row):
+            for c in range(board.col):
+                checker = board.board[r][c]
+                if checker.is_king():
+                    kingdoms[colors_dict[checker.get_color()]] += 1
+                if (r == board.row - 1 or r == 0 or c == board.col - 1 or c == 0) \
+                        and checker.get_color() != ".":
+                    kingdoms[colors_dict[checker.get_color()+2]] += 1
+        return tuple(kingdoms)
